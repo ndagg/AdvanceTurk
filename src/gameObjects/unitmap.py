e7798bbcc7e454ddb22a8ab63c8e9a472172637b
ndagg <n.daggitt@btinternet.com>
2026-04-08 20:10:15 +0100

    minor edits to move calcs

--- src/gameObjects/unitmap.py
+++ src/gameObjects/unitmap.py
@@ -9,6 +9,7 @@ import networkx as nx
 from src.gameUtils.indirect_range import generate_indirect_attack_tiles
 from src.gameUtils.aw_lists import ANY_ATTACK
 
+
 class UnitMap():
     """
     A map class for determining what moves a unit can make
@@ -23,16 +24,15 @@ class UnitMap():
         The list of movement type graphs for the map
     dims : list
         The x and y dimensions of the map
-
     """
-    
+
     def __init__(self, base_map, unit_dict: dict):
         self.super_graph = base_map.super_graph
         self.pure_terrain_graphs = base_map.sub_graphs
         self.dims = base_map.dims
         self.friendly_units = unit_dict
         self.enemy_units = {}
-        
+
         # TODO - add capture, load, hide, and delete moves
 
     def update_move_graphs(self):
@@ -46,14 +46,14 @@ class UnitMap():
                 if unit.glocation in g:
                     g.remove_node(unit.glocation)
             self.terrain_graphs.append(g)
-    
+
     def update_enemy_units(self, enemy_units: dict):
         """
         Update the list of enemy units and update the movement graphs accordingly
         """
         self.enemy_units = enemy_units
         self.update_move_graphs()
-    
+
     def generate_single_unit_move(self, unit: object) -> tuple[list, list]:
         """
         Generate a subgraph showing the possible moves for a unit
@@ -80,11 +80,11 @@ class UnitMap():
             # att = super_graph.subgraph
         att = []
         for t in att_tiles:
-            for eunit in self.enemy_units.values():
+            for eunit_ind, eunit in self.enemy_units.items():
                 if not ANY_ATTACK[unit.id, eunit.id]:
                     continue  # skip if unit can't attack this type
                 if t == eunit.glocation:
-                    att.append(t)
+                    att.append((t, eunit_ind))
                     break
 
         # unit.moves = movetype_graph.subgraph(no_att).copy()
@@ -92,7 +92,7 @@ class UnitMap():
         # return unit.moves, unit.attacks
 
         return no_att, att
-    
+
     def generate_direct_attack_tiles(self, moveable_tiles: list) -> list:
         """
         Get attackable tiles for direct units given a list of moveable tiles
