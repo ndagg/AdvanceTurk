44b9e2b63cd52a6159a00b25b14346e5a5e3d5a4
ndagg <n.daggitt@btinternet.com>
2026-05-01 20:24:50 +0100

    broaden move types

--- src/gameObjects/moves.py
+++ src/gameObjects/moves.py
@@ -4,25 +4,69 @@ Created on Sat Nov  8 15:52:24 2025
 
 @author: ndagg
 """
+from src.gameObjects.units import Infantry, Mech
 
-
-class Move():
+class Action():
     """
-    A class for storing moves to be acted on by decision-making logic
+    An abstract class covering each of the possible player actions
+    to be acted on by decision-making logic
+    """
+    def __init__(self):
+        self._id = hex(id(self))
+
+class Move(Action):
+    """
+    A class for storing unit moves and attacks
     """
     
     def __init__(self, unit: object, destination: int, fuel: int, attack_target: object=None):
+        super().__init__(self)
         self.unit = unit
         self.destination = destination
         self.fuel_cost = fuel
         self.attack_target = attack_target
-        self._id = hex(id(self))
     
     def __repr__(self):
         return f"Move(unit: {self.unit.__class__.__name__}, dest: {self.destination}, attack: {self.attack_target})"
     
 
-class EndTurn():
+class EndTurn(Action):
     """
     A class for representing an end turn
     """
+    def __init__(self):
+        super().__init__(self)
+
+
+class Cature(Action):
+    """
+    A class for representing infantry captures
+    """
+    def __init__(self, unit: object):
+        super().__init__(self)
+        # TODO - Gamestate will have to track captures
+
+
+class COPower(Action):
+    """
+    A class for representing COPower activations
+    """
+    def __init__(self):
+        super().__init__(self)
+        # TODO
+
+class SuperPower(Action):
+    """
+    A class for representing super-power activations
+    """
+    def __init__(self):
+        super().__init__(self)
+        # TODO
+
+class BuildUnit(Action):
+    """
+    A class for representing creating new units
+    """
+    def __init__(self, unit_type: type, tile: int):
+        super().__init__(self)
+        # TODO
